22f9babe05105f46e82c225e7c39a80bb586f21e
Christian Chavarro <christian@clutchprep.com>
2018-02-16 16:36:37 -0600

    Added solution for repeating artists

--- routes/profileController.js
+++ routes/profileController.js
@@ -5,16 +5,30 @@ const User = require('../models/User');
 
 profileController.post('/api/profile/artists', (req, res) => {
   req.body.map(artist => {
-    const newArtist = new Artist({
-      name: artist.name,
-      image: artist.image
-    })
-    User.findById(req.user._id)
-      .exec((err, user) => {
-        user.artists.push(newArtist._id)
-        user.save();
+    Artist.find({ name: artist.name})
+      .exec((err, artist) => {
+        // console.log('ARTIST', artist)
+        if (!artist) {
+          // console.log('Artist not found')
+          const newArtist = new Artist({
+            name: artist.name,
+            image: artist.image
+          })
+          User.findById(req.user._id)
+          .exec((err, user) => {
+            user.artists.push(newArtist._id)
+            user.save();
+          })
+          newArtist.save()
+        } else if (artist) {
+          // console.log('Artist exists')
+          User.findById(req.user._id)
+          .exec((err, user) => {
+            user.artists.push(artist._id)
+            user.save();
+          })
+        }
       })
-    newArtist.save()
   })
   res.send('Done')
 })
