d369bbf8749cdebd4c2493ee16462756868ae6e6
Christian Chavarro <christian@clutchprep.com>
2018-02-16 20:40:20 -0600

    Completed first description panel

--- client/src/Components/Onboarding.js
+++ client/src/Components/Onboarding.js
@@ -9,10 +9,12 @@ class Onboarding extends Component {
             <div className="onboardingHeader">
               {"Here's how SideBro works, " + (this.props.auth !== null ? `${this.props.auth.name}.`: "Pal.")}
             </div>
-            <div className="col s8">
+            <div className="col s8 onboardingCardContainer">
               <UserCard
                 name="Christian Chavarro"
                 bio="I created this app and I'd like to think I'm a cool guy"
+                optional="devCard"
+                onClick={() => console.log('Clicked card')}
               />
               <UserCard
                 name="Bob Smith"
@@ -30,7 +32,15 @@ class Onboarding extends Component {
                 optional="dummyUserCard"
               />
             </div>
-            <div className="col s4">
+            <div className="col s4 onboardingDescriptionDiv">
+              <div className="onboardingDescription1">
+                <ul style={{ marginBottom: '0px' }}>
+                  <li>{`You'll get a list of all available bros on the main dashboard`}</li>
+                  <li>{`Each man card displays that bro's name and their personal summary`}</li>
+                  <li>{`If you want to get more info on a specific bro, just click on their card`}</li>
+                  <li style={{ textAlign: 'center', fontWeight: '600' }}>{`Try clicking on the first card now!`}</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
